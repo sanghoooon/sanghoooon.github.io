f7c92030c66089e1f9a42ee7c9774661de5e5c2a
이상훈 <sanghoon@iMac.local>
2021-08-05 02:52:53 +0900

    rev

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -11,8 +11,7 @@ if (navigator.userAgent.match(/iPhone/i)) {
 function gestureStart() {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
-      // metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
     }
   }
 }
